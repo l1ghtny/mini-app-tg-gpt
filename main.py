dc7c66a026e78f90070231f411c6f9c653c9ed90
l1ghtny <ankoshenkov@gmail.com>
2025-12-03 14:17:18 +0000

    Fixed CORS for the new URL with proxy

--- main.py
+++ main.py
@@ -33,7 +33,8 @@ origins = [
     "http://localhost:4173",
     "http://127.0.0.1:4173",
     "https://gpt-mini-app.lightny.pro",
-    "http://192.168.1.137:5173"
+    "http://192.168.1.137:5173",
+    "https://gpt-mini-app-ru.lightny.pro"
 ]
 
 
